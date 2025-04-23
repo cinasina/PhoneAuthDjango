a9cdff7ec344b7de47be4d039ae4d6fc966f4f0b
Sina <65864255+cinasina@users.noreply.github.com>
2025-04-23 21:18:32 +0330

    Update - Accounts App - urls.py - Create "urlpatterns"

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("verify/otp/", views.VerifyOTPView.as_view(), name="verify-otp"),
+    path("set-password/", views.SetPasswordView.as_view(), name="set-password"),
+    path(
+        "user-info/", views.CompleteProfileView.as_view(), name="complete-profile"
+    ),
+    path("login/", views.LoginView.as_view(), name="login"),
+]
