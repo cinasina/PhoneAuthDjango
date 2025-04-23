5616dec787226f020473cb11222499ed1f941351
Sina <65864255+cinasina@users.noreply.github.com>
2025-04-23 21:35:33 +0330

    Update - Main urls.py - Add "swagger" Paths

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+
+    # Swagger and schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
+
