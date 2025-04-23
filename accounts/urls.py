1a44a4b0e1e772cf083ce832da867ff5a3dcc053
Sina <65864255+cinasina@users.noreply.github.com>
2025-04-23 21:36:08 +0330

    Update - Main urls.py - Add "swagger" Paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("verify/otp/", views.VerifyOTPView.as_view(), name="verify-otp"),
     path("set-password/", views.SetPasswordView.as_view(), name="set-password"),
-    path(
-        "user-info/", views.CompleteProfileView.as_view(), name="complete-profile"
-    ),
+    path("user-info/", views.CompleteProfileView.as_view(), name="complete-profile"),
     path("login/", views.LoginView.as_view(), name="login"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -22,9 +22,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-
     # Swagger and schema
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
 ]
-
