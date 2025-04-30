fcf887b4a3566671bb94c1091be41da5cb727cfb
Sina <65864255+cinasina@users.noreply.github.com>
2025-04-30 22:21:43 +0330

    Refactor Blocker App - logics.py - Update "check_failed_attempts"

--- blocker/logics.py
+++ blocker/logics.py
@@ -1,4 +1,6 @@
-from django.utils.timezone import timezone, timedelta
+from django.utils import timezone
+from datetime import timedelta
+
 from rest_framework import exceptions
 
 from blocker import models
@@ -6,15 +8,16 @@ from blocker.messages import Messages
 
 
 def check_failed_attempts(identifier, max_attempts=3, block_duration=3600):
+    ip = identifier["ip"]
     attempt, created = models.FailedAttempt.objects.get_or_create(
-        ip=identifier["ip"],
-        mobile_number=identifier.get("mobile_number"),
-        defaults={"attempts": 0},
+        ip=ip,
+        defaults={"attempts": 0, "mobile_number": identifier.get("mobile_number", "")},
     )
     if attempt.is_blocked():
         raise exceptions.ParseError({"message": Messages.BLOCK.TOO_MANY_ATTEMPTS})
     attempt.attempts += 1
     if attempt.attempts >= max_attempts:
         attempt.blocked_until = timezone.now() + timedelta(seconds=block_duration)
+    attempt.mobile_number = identifier.get("mobile_number", "")
     attempt.save()
     return attempt
